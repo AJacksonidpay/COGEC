7a1dae4a4f0608ac7da0a02626ff037848088f8a
AJacksonidpay <antoniojackson84@gmail.com>
2025-09-10 10:29:24 -0300

    Create Logo.jsx

---
+++ src/assets/Logo.jsx
@@ -0,0 +1,10 @@
+export default function Logo() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="60" height="60" viewBox="0 0 256 228">
+      <path
+        fill="#00D8FF"
+        d="M210.483 73.824a171.49 171.49 0 0 0-8.24-2.597c..."
+      />
+    </svg>
+  )
+}
